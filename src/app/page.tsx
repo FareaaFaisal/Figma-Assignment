6c8cafc4f894b4953294e46cd7cd452f4af5d6d6
FareaaFaisal <faisalaftab326@gmail.com>
2024-11-02 15:47:40 +0500

    issues fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
  </div>
  <div className="About-text">
  <h1 className="aboutheading">About</h1>
- <p className="aboutpara">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+ <p className="aboutpara">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
  <img src="/readmorebutton.png" alt="read more button"/><br/><br/><br/><br/><br/><br/><br/>
  </div>
  
